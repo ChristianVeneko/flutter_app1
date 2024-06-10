17584c4b87c0fd487cde96ba80b12275f4aa9e7f
Christian Mendoza <christianmendozater@gmail.com>
2024-06-09 23:12:12 -0400

    Homescreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Text('OLI MUNDO'),
+      home: HomeScreen(),
         
-      ),
-
-    );
+      );
   }
 }
